7a790e1af7a23cf4147e2d880eb567de0d19adde
Milan Kačarević <milan.kacareviic@gmail.com>
2026-01-19 13:57:03 +0100

    feature: add routes and shortest paths between them

--- webapp/src/app/features/registered/components/ride-booking-sidebar/ride-booking-sidebar.ts
+++ webapp/src/app/features/registered/components/ride-booking-sidebar/ride-booking-sidebar.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { LocationSearchInput } from "../../../shared/components/location-search-input/location-search-input";
+import { NominatimResult } from '../../../shared/services/nominatim.service';
+// sidebar inputs connected with state service
+import { RideBookingStateService } from '../../../../core/services/ride-booking-state.service';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'ride-booking-sidebar',
-  imports: [LocationSearchInput],
+  imports: [LocationSearchInput, NgOptimizedImage],
   templateUrl: './ride-booking-sidebar.html',
 })
 export class RideBookingSidebar {
-  pickup: String = '';
-  dropoff: String = '';
+  constructor(public bookingState: RideBookingStateService) {}
+
+  onPickupSelected(res: NominatimResult) {
+    this.bookingState.setPickup(res);
+  }
+
+  onDropoffSelected(res: NominatimResult) {
+    this.bookingState.setDropoff(res);
+  }
+
+  addStop() {
+    this.bookingState.addStop();
+  }
+
+  onStopSelected(index: number, res: NominatimResult) {
+    this.bookingState.setStop(index, res);
+  }
+
+  removeStop(index: number) {
+    this.bookingState.removeStop(index);
+  }
 }
