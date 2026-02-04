a8cdf52ae7473b3957e47d5e223788cc0ce1d7d5
loife <andrejdobric7@gmail.com>
2026-02-04 01:05:52 +0100

    Add user repository - Adds persistance layer for user. - Connects logging in and signing out to user repo. - Navigates based on stored user in splash screen.

--- mobileapp/app/src/main/java/com/example/mobileapp/core/UberPlusApp.java
+++ mobileapp/app/src/main/java/com/example/mobileapp/core/UberPlusApp.java
@@ -3,6 +3,7 @@ package com.example.mobileapp.core;
 import android.app.Application;
 
 import com.example.mobileapp.core.network.ApiClient;
+import com.example.mobileapp.features.shared.repositories.UserRepository;
 
 public class UberPlusApp extends Application {
     @Override
@@ -10,5 +11,6 @@ public class UberPlusApp extends Application {
         super.onCreate();
 
         ApiClient.initialize(this);
+        UserRepository.initialize(this);
     }
 }
